2009a7cb41986826f06529c7766dcf9827bd07fc
Lars <larslpt@gmail.com>
2021-11-06 10:47:02 +0100

    Fragen überarbeitet & Kontakt Formular

--- src/components/owner/Owner.tsx
+++ src/components/owner/Owner.tsx
@@ -1,4 +1,4 @@
-import { CardMedia, Grid, Typography } from "@mui/material";
+import { CardMedia, formLabelClasses, Grid, Typography } from "@mui/material";
 import Questions from "../ui /questions/Questions";
 
 const steps = [
@@ -17,17 +17,18 @@ const steps = [
     label: "Frage 2 | Garage",
     showIfNo: false,
     description: "Hat Ihr Haus eine Garage?",
-    returnYes: "Sie haben mit ja geantwortet",
-    returnNo: "Sie haben mit nein geantwortet",
+    returnYes: "Garagen bietet häufig eine gute Fläche zur Begrünung. Diese können dadurch vor Witterungseinflüssen geschützt werden, und Temperaturschwankungen abfedern. ",
+    returnNo: "",
     imagePath:
       "https://www.fertiggaragen.net/media/pages/ausstattungen/dachbegruenung/16ddcf3349-1627299100/juwel-fertiggaragen-dachbegruenung.jpg",
   },
   {
     label: "Frage 3 | Terasse",
-    showIfNo: true,
+    showIfNo: false,
     description: `Hat Ihr Haus eine Terasse?`,
-    returnYes: "Sie haben mit ja geantwortet",
-    returnNo: "Sie haben mit nein geantwortet",
+    returnYes:
+      "Auch eine Terasse bietet viele Möglichkeiten Pflanzen unterzubringen.",
+    returnNo: "",
     imagePath:
       "https://i.pinimg.com/originals/85/ef/df/85efdfd38322a03d73024b9052acef91.jpg",
   },
@@ -35,22 +36,28 @@ const steps = [
     label: "Frage 4 | Begrünung",
     showIfNo: true,
     description: `Können Sie sich vorstellen Ihr Dach zu begrünen?`,
-    returnYes: "Sie haben mit ja geantwortet",
-    returnNo: "Sie haben mit nein geantwortet",
+    returnYes: "Gärtnerische Gestaltung und Begrünung von Innenhöfen, Stellplätzen und Vorgärten Zuschuss 60% der als förderfähig anerkannten Kosten (unrentierliche Kosten), die nicht durch zu erwartende Einnahmen/Erträge gedeckt werden können.",
+    returnNo: "Eine Begrünung bringt viele positive Aspekte mit sich.",
+    imagePath:
+      "https://www.ndr.de/ratgeber/garten/dachbegruenung106_v-contentgross.jpg",
   },
   {
     label: "Frage 5 | Photovoltaik",
     showIfNo: true,
     description: `Sind an ihrem Haus Photovoltaikanlagen verbaut?`,
-    returnYes: "Sie haben mit ja geantwortet",
+    returnYes: "Auch mit Photovoltaik anlagen ist Dachbegrünung möglich. ",
     returnNo: "Sie haben mit nein geantwortet",
+    imagePath:
+      "https://images.unsplash.com/photo-1613665813446-82a78c468a1d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1158&q=80",
   },
   {
     label: "Frage 6 | Projekte",
-    showIfNo: true,
+    showIfNo: false,
     description: `Können Sie sich vorstellen an öffentlichen Begrünungsprojekten teilzunehmen?`,
     returnYes: "Sie haben mit ja geantwortet",
-    returnNo: "Sie haben mit nein geantwortet",
+    returnNo: "",
+    imagePath:
+      "https://images.unsplash.com/photo-1618698343216-671a5d82b85a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1072&q=80",
   },
 ];
 
--- src/components/ui /questions/Questions.tsx
+++ src/components/ui /questions/Questions.tsx
@@ -7,8 +7,9 @@ import StepContent from "@mui/material/StepContent";
 import Button from "@mui/material/Button";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
-import { Card, Divider } from "@mui/material";
+import { Card, Divider, Grid } from "@mui/material";
 import Result from "./Result";
+import TextField from "@mui/material/TextField";
 
 interface QuestionsProps {
   steps: {
@@ -25,6 +26,7 @@ interface QuestionsProps {
 
 const Questions = ({ steps }: QuestionsProps) => {
   const [activeStep, setActiveStep] = React.useState(0);
+  const [contacted, setContacted] = React.useState(false);
 
   const handleNext = (answer: boolean) => {
     setActiveStep((prevActiveStep) => prevActiveStep + 1);
@@ -101,8 +103,8 @@ const Questions = ({ steps }: QuestionsProps) => {
             <Paper square elevation={0} sx={{ p: 3, maxWidth: "45vw" }}>
               <Typography variant="h3">Ergebnisse</Typography>
               {answers.map((val, index) => {
-                if (!steps[index].showIfNo){
-                    return <></>;
+                if (!steps[index].showIfNo) {
+                  return <></>;
                 }
                 return (
                   <Result
@@ -119,6 +121,64 @@ const Questions = ({ steps }: QuestionsProps) => {
                   </Result>
                 );
               })}
+              <Card
+                square
+                sx={{
+                  maxWidth: "100%",
+                  p: 2,
+                  backgroundColor: "#A7D930CA",
+                  my: 1.5,
+                }}
+              > {!contacted && <><Typography
+                  color="text.primary"
+                  align="left"
+                  sx={{ my: 1, pb: 0 }}
+                  gutterBottom
+                  variant="h5"
+                  component="div"
+                >
+                  Kontaktieren Sie uns!
+                </Typography>
+                <Grid container spacing={2}>
+                  <Grid item>
+                    <TextField
+                      id="standard-basic"
+                      label="Name"
+                      variant="filled"
+                      size='small'
+                      sx={{ backgroundColor: "white" }}
+                    />
+                  </Grid>
+                  <Grid item>
+                    <TextField
+                      id="standard-basic"
+                      label="E-Mail Adresse"
+                      variant="filled"
+                      size='small'
+                      sx={{ backgroundColor: "white" }}
+                    />
+                  </Grid>
+                  <Grid item></Grid>
+                </Grid>
+                <TextField
+                  sx={{ backgroundColor: "white", my:2 }}
+                  fullWidth
+                  multiline
+                  variant="standard"
+                />
+                <Button fullWidth variant="contained" onClick={event =>{ setContacted(true)}}>Senden</Button>
+            </>}
+            {contacted && <Typography
+                  color="text.primary"
+                  align="left"
+                  sx={{ my: 1, pb: 0 }}
+                  gutterBottom
+                  variant="h5"
+                  component="div"
+                >
+                 Vielen Dank! Wir melden uns bei Ihnen. 
+                </Typography>}
+              </Card>
               <Button onClick={handleReset} sx={{ mt: 1, mr: 1 }}>
                 Nochmal
               </Button>
